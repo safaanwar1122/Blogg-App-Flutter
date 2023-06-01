b43e52cdd6bb1bbc23435ab4e6635a788e8f193f
Safa <safaanwar2211@gmail.com>
2023-06-01 07:20:18 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:blogg_app_flutter/post.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.pink,
+      ),
+      home: Post(),
+    );
+  }
+}
+
